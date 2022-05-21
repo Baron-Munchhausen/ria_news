7cba14f59bd169019df974e4e16ba3336ed0e584
Leonov <mjunghauzen@gmail.com>
2022-05-21 10:54:49 +0300

    fix 21.05

--- source/ria_news/ria_news_parser/ria_news_parser.py
+++ source/ria_news/ria_news_parser/ria_news_parser.py
@@ -20,12 +20,16 @@ wks = sheets.worksheet_by_title(config['google']['article_file_sheet'])
 error_sheets = client.open_by_url(config['google']['error_file'])
 error_wks = error_sheets.worksheet_by_title(config['google']['error_file_sheet'])
 
+# Last downloaded article
 last_datetime = datetime.datetime.strptime(wks.get_value('D1'), '%H:%M %d.%m.%Y')
+last_url = wks.get_value('B1')
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/41.0.2228.0 Safari/537.3'}
 
 ria = 'https://ria.ru/services/economy/more.html?date='
+ria = 'https://ria.ru/services/search/getmore/?query=&offset='
+offset = 0
 not_end = True
 current_datetime = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
 #current_datetime = datetime.datetime(2022,5,12,9,0).strftime('%Y%m%dT%H%M%S')
@@ -35,13 +39,13 @@ articles = []
 while not_end:
 
     url_for_request = ria + current_datetime
+    url_for_request = ria + str(offset)
 
     req = requests.get(url_for_request, headers=headers)
     soup = BeautifulSoup(req.text, 'lxml')
 
     for item in soup.find_all('div', attrs={'class': 'list-item'}):
         url = item.find_all('a', attrs={'class': 'list-item__title color-font-hover-only'})[0].attrs['href']
-
         try:
             req2 = requests.get(url, headers=headers)
             soup2 = BeautifulSoup(req2.text, 'lxml')
@@ -57,13 +61,15 @@ while not_end:
         except:
             error_wks.insert_rows(0, 1, [url])
 
-        if createdAt_datetime > last_datetime:
+        if createdAt_datetime >= last_datetime and url != last_url and createdAt_datetime <= datetime.datetime.now():
             article = {'title': title, 'url': url, 'author': author, 'createdAt': createdAt, 'createdAt_datetime': createdAt_datetime}
             print(article)
             articles.append(article)
-        else:
+        elif createdAt_datetime < last_datetime:
             not_end = False
+
     current_datetime = createdAt_datetime.strftime('%Y%m%dT%H%M59')
+    offset += 20
     # not_end = False
 
 
